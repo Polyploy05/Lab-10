7d97a077768934a71a28dd4818b0a5090bf07b5f
Polyploy05 <jakada005@gmail.com>
2026-04-08 13:05:30 -0700

    Update minotaur.py

--- minotaur.py
+++ minotaur.py
@@ -1,4 +1,10 @@
-
+'''
+Name: Daniel Puerto & Jacob Miranda
+Date: 4/8/2026
+Group: 12
+Description: Creates the minotaur for use in the maze. Sets itself to a random location in the maze.
+When moving, it always knows where the hero is and will move towards the hero if possible. If not, it will move randomly.
+'''
 
 import random
 import maze
@@ -6,19 +12,23 @@ import maze
 class Minotaur:
 
     def __init__(self):
+        #New maze instance to access the maze and find a random location for the minotaur
         self.new_maze = maze.Maze()
         self.pos = None
         while self.pos is None:
-            i = random.randint(0, len(maze.Maze().maze) - 1)
-            j = random.randint(0, len(maze.Maze().maze[0]) - 1)
-            if maze.Maze().maze[i][j] == ' ':
+            #Find a location. If empty, place the minotaur there. Else, keep looking.
+            i = random.randint(0, len(self.new_maze.maze) - 1)
+            j = random.randint(0, len(self.new_maze.maze[0]) - 1)
+            if self.new_maze.maze[i][j] == ' ':
                 self.pos = [i, j]
-                maze.Maze().maze[i][j] = 'M'
-                print(self.pos)
+                self.new_maze.maze[i][j] = 'M'
     
-    def move(self):
+    def move_minotaur(self):
+        '''The move function for the minotaur. It will always know where the hero is and will move towards the hero if possible. If not, it will move randomly.
+        It does this by accesing the maze and searching for all possible moves. It then checks if any of those moves are in the direction of the hero. 
+        If so, it adds those moves to a list of good moves and picks one. If no good moves exist, it moves randomly.'''    
 
-        #Finds the hero's position as well as all 4 possibel moves for the minotaur
+        #Finds the hero's position as well as all 4 possible moves for the minotaur
         pos_hero = self.new_maze.search_maze('H')
         pos_left = [self.pos[0], self.pos[1] - 1]
         pos_right = [self.pos[0], self.pos[1] + 1]
@@ -32,36 +42,31 @@ class Minotaur:
                 valid_spaces.append(space)
         
         #Move logic. The minotaur will detect if the hero is above, below, or to the side of the hero.
-        #It attempts to move in the direction of the hero, so long as it is a valid move.
+        #Logs all of these moves as good moves.
+        good_moves = []
         if pos_hero[0] < self.pos[0] and pos_up in valid_spaces:
-            self.new_maze.maze[self.pos[0]][self.pos[1]] = ' '
-            self.pos = pos_up
-            current_obj = self.new_maze.maze[self.pos[0]][self.pos[1]]
-            self.new_maze.maze[self.pos[0]][self.pos[1]] = 'M'
-            return current_obj
-        elif pos_hero[0] > self.pos[0] and pos_down in valid_spaces:
-            self.new_maze.maze[self.pos[0]][self.pos[1]] = ' '
-            self.pos = pos_down
-            current_obj = self.new_maze.maze[self.pos[0]][self.pos[1]]
-            self.new_maze.maze[self.pos[0]][self.pos[1]] = 'M'
-            return current_obj
-        elif pos_hero[1] < self.pos[1] and pos_left in valid_spaces:
-            self.new_maze.maze[self.pos[0]][self.pos[1]] = ' '
-            self.pos = pos_left
-            current_obj = self.new_maze.maze[self.pos[0]][self.pos[1]]
-            self.new_maze.maze[self.pos[0]][self.pos[1]] = 'M'
-            return current_obj
-        elif pos_hero[1] > self.pos[1] and pos_right in valid_spaces:
-            self.new_maze.maze[self.pos[0]][self.pos[1]] = ' '
-            self.pos = pos_right
-            current_obj = self.new_maze.maze[self.pos[0]][self.pos[1]]
-            self.new_maze.maze[self.pos[0]][self.pos[1]] = 'M'
-            return current_obj
-        #If unable to move directly towards the hero, move randomly.
-        else:
+            good_moves.append(pos_up)
+        if pos_hero[0] > self.pos[0] and pos_down in valid_spaces:
+            good_moves.append(pos_down)
+        if pos_hero[1] < self.pos[1] and pos_left in valid_spaces:
+            good_moves.append(pos_left)
+        if pos_hero[1] > self.pos[1] and pos_right in valid_spaces:
+            good_moves.append(pos_right)
+
+        #If no good moves exist, move randomly. Else, move to a random good move.
+        if len(good_moves) == 0:
             random_space = random.choice(valid_spaces)
             self.new_maze.maze[self.pos[0]][self.pos[1]] = ' '
             self.pos = random_space
             current_obj = self.new_maze.maze[self.pos[0]][self.pos[1]]
             self.new_maze.maze[self.pos[0]][self.pos[1]] = 'M'
+            print(self.pos)
+            return current_obj
+        else:
+            move = random.choice(good_moves) 
+            self.new_maze.maze[self.pos[0]][self.pos[1]] = ' '
+            self.pos = move
+            current_obj = self.new_maze.maze[self.pos[0]][self.pos[1]]
+            self.new_maze.maze[self.pos[0]][self.pos[1]] = 'M'
+            print(self.pos)
             return current_obj
