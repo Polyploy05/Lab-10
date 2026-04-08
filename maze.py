0d8981bbf79db345052401089fc862c7eae281de
Polyploy05 <jakada005@gmail.com>
2026-04-08 13:12:49 -0700

    Update maze.py

--- maze.py
+++ maze.py
@@ -2,7 +2,9 @@
 Name: Daniel Puerto & Jacob Miranda
 Date: 4/8/2026
 Group: 12
-Description: Creates the maze class. Initializes the maze function. 
+Description: Creates the maze class. As a singleton, it is only initialized once and all other instances of the maze will reference the same maze. 
+It reads the maze from a text file and stores it as a 2D list. Overites str, getitem, and len for easy access and printing. 
+Also has a search function to find the location of any character in the maze.
 '''
 
 class Maze():
@@ -11,6 +13,7 @@ class Maze():
 
     
     def __new__(cls, *args):
+        #Checks if an instance of the maze already exists. If not, creates one. Else, returns the existing instance.
         if cls._instance is None:
             cls._instance = super(Maze, cls).__new__(cls)
         return cls._instance
@@ -35,13 +38,16 @@ class Maze():
 
 
     def __str__(self):
+        #Prints out the maze without list formatting, 1 row per line
         return '\n'.join(''.join(row) for row in self.maze)
     
 
     def search_maze(self, char):
+        #Searches the maze for a character and returns its position as a list [row, col]. If not found, returns None.
         for i in range(len(self.maze)):
             for j in range(len(self.maze[i])):
                 if self.maze[i][j] == char:
                     return [i, j]
         return None
+
 
