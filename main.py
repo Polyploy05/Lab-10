de21c746a0d1a06baad32ca79a28507dfc1129d5
DMOC1211 <145091883+DMOC1211@users.noreply.github.com>
2026-04-08 12:19:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,50 @@ import minotaur
 import hero
 
 def main():
-    new_maze = maze.Maze()
-    min = minotaur.Minotaur()
-    her = hero.Hero()
+    #Objects creation
+    maze = maze.Maze()
+    minotaur = minotaur.Minotaur()
+    hero = hero.Hero()
 
+    #Game Loop
+    while True:
+        #Displays the Maze
+        print(maze)
 
-    print(new_maze)
+        #User input
+        move = input("Move (w/a/s/d):).lower()
 
-    print(new_maze.search_maze('s'))
+        #Move Hero
+        if move == 'w':
+            result = hero.go_up()
+        elif move == 's':
+            result = hero.go_down()
+        elif move == 'a':
+            result = hero.go_left()
+        elif move == 'd':
+            result = hero.go_right()
+        else:
+            print("Invalid input.")
+            continue
 
+        #Check result after hero move
+        if result == 'f':
+            print(maze)
+            print("You escaped the maze! You win!")
+            break
+        elif result == 'M':
+            print(maze)
+            print("The Minotaur got you! You lost!")
+            break
+
+        #Minotaur Moves AFTER valid hero move
+        result = minotaur.move_minotaur()
+
+        #Check result after minotaur move
+        if result =='H':
+            print(maze)
+            print("The Minotaur caught you! You lost!")
+            break
+            
 if __name__ == "__main__":
     main()
